717674191952d8d4f402346717465f710ab64196
Campbell Crowley <github@campbellcrowley.com>
2018-07-21 13:17:58 -0700

    canvascontroller.js passes eslint

--- canvascontroller.js
+++ canvascontroller.js
@@ -2,18 +2,17 @@
 // Author: Campbell Crowley (web@campbellcrowley.com)
 (function(TraX, undefined) {
 (function(Canvases, undefined) {
-
 // Number of points to draw to show a line.
 const numAccelPoints = 15;
 // The delta angle between points to draw a sphere in radians.
 const pointAngleDelta = Math.PI / 5.0;
 
 // DOM Elements.
-var gyroCanvas, accelCanvas;
+let gyroCanvas; let accelCanvas;
 // Canvas contexts.
-var gyroCtx, accelCtx;
+let gyroCtx; let accelCtx;
 // Sphere being drawn in canvas.
-var sphere;
+let sphere;
 // Rotation where -Z is down.
 TraX.downRotation = {a: 0, b: 0, g: 0};
 // Current given rotation of device.
@@ -23,21 +22,21 @@ Canvases.drawAccel = [0, 0, 0];
 // Vector where +X is forwards.
 Canvases.forwardVector = [0, 1, 0];
 // Accumulator for resetting forward vector.
-var forwardAcc = 0;
+let forwardAcc = 0;
 // Calculated offset between what we think is forwards and the direction GPS
 // tells us we are going.
 Canvases.headingOffset = 0;
 // The current rate at which the device is rotating.
 Canvases.rotationRate = {a: 0, b: 0, g: 0};
 // The distance the sphere is away from front of page. (Depth into screen)
-var distance = 100;
+let distance = 100;
 // Dimensions of canvases.
-var gyroWidth, gyroHeight, accelWidth, accelHeight;
+let gyroWidth; let gyroHeight; let accelWidth; let accelHeight;
 
 // Initialize
 Canvases.init = function() {
-  gyroCanvas = document.getElementById("realtimeGyroSphere");
-  accelCanvas = document.getElementById("realtimeAccelCanvas");
+  gyroCanvas = document.getElementById('realtimeGyroSphere');
+  accelCanvas = document.getElementById('realtimeAccelCanvas');
   gyroCtx = gyroCanvas.getContext('2d');
   accelCtx = accelCanvas.getContext('2d');
   gyroWidth = gyroCanvas.width;
@@ -45,7 +44,7 @@ Canvases.init = function() {
   accelWidth = accelCanvas.width;
   accelHeight = accelCanvas.height;
 
-  sphere = new Canvases.sphere3d(50);
+  sphere = new Canvases.Sphere3d(50);
 
   // Render one frame.
   Canvases.renderGyro();
@@ -53,15 +52,15 @@ Canvases.init = function() {
 };
 
 // Class storing information about how to draw a sphere on the canvas.
-Canvases.sphere3d = function(radius) {
+Canvases.Sphere3d = function(radius) {
   this.point = [];
-  this.color = "#000000";
+  this.color = '#000000';
   this.radius = (typeof radius === 'undefined') ? 20.0 : radius;
   this.radius = (typeof radius !== 'number') ? 20.0 : radius;
   this.numberOfVertexes = 0;
 
-  for (var alpha = 0.0; alpha < 2.0 * Math.PI; alpha += pointAngleDelta) {
-    var point = new TraX.Common.Point3d();
+  for (let alpha = 0.0; alpha < 2.0 * Math.PI; alpha += pointAngleDelta) {
+    let point = new TraX.Common.Point3d();
 
     point.x = Math.cos(alpha) * this.radius;
     point.y = 0;
@@ -70,14 +69,14 @@ Canvases.sphere3d = function(radius) {
     this.point[this.numberOfVertexes++] = point;
   }
 
-  for (var direction = 1; direction >= -1; direction -= 2) {
-    for (var beta = pointAngleDelta; beta < Math.PI / 2.0 - pointAngleDelta;
+  for (let direction = 1; direction >= -1; direction -= 2) {
+    for (let beta = pointAngleDelta; beta < Math.PI / 2.0 - pointAngleDelta;
          beta += pointAngleDelta) {
-      var r = Math.cos(beta) * this.radius;
-      var Y = Math.sin(beta) * this.radius * direction;
+      let r = Math.cos(beta) * this.radius;
+      let Y = Math.sin(beta) * this.radius * direction;
 
-      for (var alpha = 0; alpha < Math.PI * 2.0; alpha += pointAngleDelta) {
-        var point = new TraX.Common.Point3d();
+      for (let alpha = 0; alpha < Math.PI * 2.0; alpha += pointAngleDelta) {
+        let point = new TraX.Common.Point3d();
 
         point.x = Math.cos(alpha) * r;
         point.y = Y;
@@ -88,44 +87,44 @@ Canvases.sphere3d = function(radius) {
     }
   }
 };
-// Project a point onto the screen plane.
+/**
+ * Project a point onto the screen plane.
+ *
+ * @private
+ * @param {number} xy The X or Y scalar of the point to project.
+ * @param {number} z The Z scalar of the point.
+ * @param {number} xyOffset The offset of the X or Y scalar.
+ * @param {number} zOffset Offset the Z scalar.
+ * @param {number} distance Multiplies the X or Y scalar.
+ * @return {number} The projected X or Y scalar.
+ */
 function projection(xy, z, xyOffset, zOffset, distance) {
   return (distance * xy) / (z - zOffset) + xyOffset;
 }
-
-function arrayToRad(arr) {
-  var newArr = [];
-  for (var i = 0; i < arr.length; i++) {
-    newArr.push(arr[i] / 180.0 * Math.PI);
-  }
-  return newArr;
-}
-function arrayToDeg(arr) {
-  var newArr = [];
-  for (var i = 0; i < arr.length; i++) {
-    newArr.push(arr[i] * 180.0 / Math.PI);
-  }
-  return newArr;
-}
-// Render the canvas showing orientation/gyro data.
+/**
+ * Render the canvas showing orientation/gyro data.
+ *
+ * @param {boolean} [makeDown=false] Make the orientation sphere point down to
+ * world down, or screen down.
+ */
 Canvases.renderGyro = function(makeDown) {
   gyroCtx.save();
   gyroCtx.clearRect(0, 0, gyroWidth, gyroHeight);
-  var rotatedPoints = [];
+  let rotatedPoints = [];
 
-  var rotation =
+  let rotation =
       [Canvases.rotation.a, Canvases.rotation.b, Canvases.rotation.g];
 
   // TODO: Nothing differnet anymore. Remove?
-  var downRotationAdjusted = {
+  let downRotationAdjusted = {
     a: TraX.downRotation.a,
     b: TraX.downRotation.b,
-    g: TraX.downRotation.g
+    g: TraX.downRotation.g,
   };
 
   // Gyro Sphere
-  for (var i = 0; i < sphere.numberOfVertexes; i++) {
-    var point = new TraX.Common.Point3d(sphere.point[i]);
+  for (let i = 0; i < sphere.numberOfVertexes; i++) {
+    let point = new TraX.Common.Point3d(sphere.point[i]);
 
     point = TraX.Common.rotateVector(point, rotation, makeDown);
 
@@ -133,10 +132,10 @@ Canvases.renderGyro = function(makeDown) {
   }
 
   // Calculated down vector
-  for (var i = 0; i < numAccelPoints; i++) {
-    var percent = i / numAccelPoints;
-    var point =
-        new TraX.Common.Point3d(0, 0, -sphere.radius * percent, "magenta");
+  for (let i = 0; i < numAccelPoints; i++) {
+    let percent = i / numAccelPoints;
+    let point =
+        new TraX.Common.Point3d(0, 0, -sphere.radius * percent, 'magenta');
 
     point = TraX.Common.rotateVector(point, downRotationAdjusted, false);
     point = TraX.Common.rotateVector(point, rotation, makeDown);
@@ -145,10 +144,10 @@ Canvases.renderGyro = function(makeDown) {
   }
   if (!makeDown) {
     // Measured down vector
-    for (var i = 0; i < numAccelPoints; i++) {
-      var percent = i / numAccelPoints;
-      var point =
-          new TraX.Common.Point3d(0, 0, -sphere.radius * percent, "green");
+    for (let i = 0; i < numAccelPoints; i++) {
+      let percent = i / numAccelPoints;
+      let point =
+          new TraX.Common.Point3d(0, 0, -sphere.radius * percent, 'green');
 
       point = TraX.Common.rotateVector(point, downRotationAdjusted, false);
 
@@ -156,12 +155,12 @@ Canvases.renderGyro = function(makeDown) {
     }
   }
   // Calculated forwards vector
-  for (var i = 0; i < numAccelPoints; i++) {
-    var percent = i / numAccelPoints;
-    var point = new TraX.Common.Point3d(
+  for (let i = 0; i < numAccelPoints; i++) {
+    let percent = i / numAccelPoints;
+    let point = new TraX.Common.Point3d(
         Canvases.forwardVector[0] * sphere.radius * percent,
         Canvases.forwardVector[1] * sphere.radius * percent,
-        Canvases.forwardVector[2] * sphere.radius * percent, "blue");
+        Canvases.forwardVector[2] * sphere.radius * percent, 'blue');
 
     point = TraX.Common.rotateVector(point, downRotationAdjusted, true);
     point = TraX.Common.rotateVector(point, rotation, makeDown);
@@ -171,9 +170,9 @@ Canvases.renderGyro = function(makeDown) {
 
   // Flip z and y axes so down is down on the screen, not into the screen.
   if (makeDown) {
-    for (var i = 0; i < rotatedPoints.length; i++) {
+    for (let i = 0; i < rotatedPoints.length; i++) {
       // TraX.Common.rotateZ(rotatedPoints[i], Canvases.headingOffset);
-      var tmp = new TraX.Common.Point3d(rotatedPoints[i]);
+      let tmp = new TraX.Common.Point3d(rotatedPoints[i]);
       rotatedPoints[i].y = tmp.z;
       rotatedPoints[i].z = -tmp.y;
     }
@@ -183,11 +182,11 @@ Canvases.renderGyro = function(makeDown) {
   sortPoints(rotatedPoints);
 
   // Draw all points.
-  for (var i = 0; i < rotatedPoints.length; i++) {
-    var point = rotatedPoints[i];
+  for (let i = 0; i < rotatedPoints.length; i++) {
+    let point = rotatedPoints[i];
 
-    var x = projection(point.x, point.z, gyroWidth / 2.0, 100.0, distance);
-    var y = projection(point.y, point.z, gyroHeight / 2.0, 100.0, distance);
+    let x = projection(point.x, point.z, gyroWidth / 2.0, 100.0, distance);
+    let y = projection(point.y, point.z, gyroHeight / 2.0, 100.0, distance);
 
     if (x >= 0 && x < gyroWidth) {
       if (y >= 0 && y < gyroHeight) {
@@ -201,19 +200,29 @@ Canvases.renderGyro = function(makeDown) {
   }
   gyroCtx.restore();
 };
-// Sort points by z-index to draw things in back first.
+/**
+ * Sort points by z-index to draw things in back first. Points are sorted
+ * in-place.
+ *
+ * @param {Array.<TraX~Common~Point3d>} points The points to sort.
+ */
 function sortPoints(points) {
   points.sort(function(a, b) {
     return (a.z == b.z) ? 0 : ((a.z < b.z) ? -1 : 1);
   });
 }
-// Draw data on canvas showing acceleration data.
+/**
+ * Draw data on canvas showing acceleration data.
+ *
+ * @param {boolean} [externalDevice=false] Is this device a different device
+ * from the one recording the data.
+ */
 Canvases.renderAccel = function(externalDevice) {
   accelCtx.save();
   accelCtx.clearRect(0, 0, accelWidth, accelHeight);
 
   // Border
-  accelCtx.strokeStyle = "rgba(0, 0, 0, 1.0)";
+  accelCtx.strokeStyle = 'rgba(0, 0, 0, 1.0)';
   accelCtx.beginPath();
   accelCtx.rect(
       accelWidth * 0.05, accelHeight * 0.05, accelWidth * 0.90,
@@ -222,7 +231,7 @@ Canvases.renderAccel = function(externalDevice) {
   accelCtx.closePath();
 
   // 1G guide ring
-  accelCtx.strokeStyle = "rgba(125, 125, 125, 0.6)";
+  accelCtx.strokeStyle = 'rgba(125, 125, 125, 0.6)';
   accelCtx.beginPath();
   accelCtx.arc(
       accelWidth * 0.5, accelHeight * 0.5, accelWidth * 0.90 * 3.0 / 4.0 / 2.0,
@@ -238,37 +247,34 @@ Canvases.renderAccel = function(externalDevice) {
   accelCtx.closePath();
 
   // Acceleration vector3d
-  var point = new TraX.Common.Point3d(
+  let point = new TraX.Common.Point3d(
       -Canvases.drawAccel[0], Canvases.drawAccel[1], Canvases.drawAccel[2]);
 
   point = TraX.Common.rotateVector(point, Canvases.rotation, true);
-  var Ax_ = point.x;
-  var Ay_ = point.y;
-  var Az_ = point.z;
 
-  var more = 0;
+  let more = 0;
   if (screen && screen.orientation && !externalDevice) {
     more = -screen.orientation.angle || 0;
     more /= 180.0 / Math.PI;
   }
   TraX.Common.rotateZ(point, -Canvases.rotation.a + more);
 
-  var Ax = point.x;
-  var Ay = point.y;
-  var Az = point.z + Agrav;
+  let Ax = point.x;
+  let Ay = point.y;
+  let Az = point.z + Agrav;
 
   // Draw lateral acceleration line/points.
-  for (var i = 0; i < numAccelPoints; i++) {
-    var percent = i / numAccelPoints;
-    var x = accelWidth / 2.0 +
+  for (let i = 0; i < numAccelPoints; i++) {
+    let percent = i / numAccelPoints;
+    let x = accelWidth / 2.0 +
         Ax / Agrav / 2.0 * 0.90 * percent * accelWidth * 3.0 / 4.0;
-    var y = accelHeight / 2.0 +
+    let y = accelHeight / 2.0 +
         Ay / Agrav / 2.0 * 0.90 * percent * accelHeight * 3.0 / 4.0;
-    drawPoint(accelCtx, x, y, 3, "red");
+    drawPoint(accelCtx, x, y, 3, 'red');
   }
 
   // Draw vertical acceleration bar.
-  accelCtx.fillStyle = Az < 0 ? "rgb(255, 50, 50)" : "red";
+  accelCtx.fillStyle = Az < 0 ? 'rgb(255, 50, 50)' : 'red';
   accelCtx.rect(
       accelWidth * 0.95, accelHeight * 0.50, accelWidth * 0.05,
       Az / Agrav / 2.0 * 0.90 * 3.0 / 4.0 * accelHeight);
@@ -277,8 +283,8 @@ Canvases.renderAccel = function(externalDevice) {
   accelCtx.restore();
 
   if (!externalDevice) {
-    var accelMag = Math.abs(Math.hypot(Ax, Ay, Az));
-    var rotMag = Math.abs(
+    let accelMag = Math.abs(Math.hypot(Ax, Ay, Az));
+    let rotMag = Math.abs(
         Math.hypot(
             Canvases.rotationRate.a, Canvases.rotationRate.b,
             Canvases.rotationRate.g));
@@ -298,7 +304,15 @@ Canvases.renderAccel = function(externalDevice) {
   }
 };
 
-// Draw a point on the canvas with given settings.
+/**
+ * Draw a point on the canvas with given settings.
+ *
+ * @param {Canvas~Context2d} ctx The canvas context to draw on.
+ * @param {number} x The X coordinate to draw the point.
+ * @param {number} y The Y coordinate to draw the point.
+ * @param {number} size The size of the point to draw.
+ * @param {string} color The color to make the point.
+ */
 function drawPoint(ctx, x, y, size, color) {
   ctx.save();
   ctx.beginPath();
@@ -312,16 +326,15 @@ Canvases.resetEstimatedVelocity = function(gpsSpeed, gpsHeading) {
   // Need to be moving faster than 1m/s, otherwise the UI will go a little
   // crazy.
   if (gpsSpeed < 1.0) return;
-  var newHeading = gpsHeading / 180.0 * Math.PI;
+  let newHeading = gpsHeading / 180.0 * Math.PI;
 
-  var point =
+  let point =
       TraX.Common.rotateVector(Canvases.forwardVector, TraX.downRotation, true);
   point = TraX.Common.rotateVector(point, Canvases.rotation, false);
 
-  var estimatedHeading = Math.atan2(point.y, point.x);
+  let estimatedHeading = Math.atan2(point.y, point.x);
 
   Canvases.headingOffset = newHeading - estimatedHeading;
 };
-
 }(window.TraX.Canvases = window.TraX.Canvases || {}));
 }(window.TraX = window.TraX || {}));
