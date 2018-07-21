19f4151645607b43d550de504be941feffdaccf4
Campbell Crowley <github@campbellcrowley.com>
2018-07-21 13:28:01 -0700

    keepAwake.js passes eslint

--- keepAwake.js
+++ keepAwake.js
@@ -1,16 +1,22 @@
-var Util = {};
+let Util = {};
 Util.base64 = function(mimeType, base64) {
   return 'data:' + mimeType + ';base64,' + base64;
 };
 
 // Create video to play on loop in order to keep device awake.
-var video = document.createElement('video');
+let video = document.createElement('video');
 video.loop = true;
 document.body.appendChild(video);
 
-// Add data to for video to play.
+/**
+ * Add data to for video to play.
+ *
+ * @param {Element} element The parent video element to attach the source to.
+ * @param {string} type The mimeType of the source.
+ * @param {string} dataURI The src value.
+ */
 function addSourceToVideo(element, type, dataURI) {
-  var source = document.createElement('source');
+  let source = document.createElement('source');
   source.src = dataURI;
   source.type = 'video/' + type;
   element.appendChild(source);
@@ -20,27 +26,55 @@ addSourceToVideo(
     video, 'webm',
     Util.base64(
         'video/webm',
-        'GkXfo0AgQoaBAUL3gQFC8oEEQvOBCEKCQAR3ZWJtQoeBAkKFgQIYU4BnQI0VSalmQCgq17FAAw9CQE2AQAZ3aGFtbXlXQUAGd2hhbW15RIlACECPQAAAAAAAFlSua0AxrkAu14EBY8WBAZyBACK1nEADdW5khkAFVl9WUDglhohAA1ZQOIOBAeBABrCBCLqBCB9DtnVAIueBAKNAHIEAAIAwAQCdASoIAAgAAUAmJaQAA3AA/vz0AAA='));
+        'GkXfo0AgQoaBAUL3gQFC8oEEQvOBCEKCQAR3ZWJtQoeBAkKFgQIYU4BnQI0VSalmQCgq' +
+            '17FAAw9CQE2AQAZ3aGFtbXlXQUAGd2hhbW15RIlACECPQAAAAAAAFlSua0AxrkAu' +
+            '14EBY8WBAZyBACK1nEADdW5khkAFVl9WUDglhohAA1ZQOIOBAeBABrCBCLqBCB9D' +
+            'tnVAIueBAKNAHIEAAIAwAQCdASoIAAgAAUAmJaQAA3AA/vz0AAA='));
 addSourceToVideo(
     video, 'mp4',
     Util.base64(
         'video/mp4',
-        'AAAAHGZ0eXBpc29tAAACAGlzb21pc28ybXA0MQAAAAhmcmVlAAAAG21kYXQAAAGzABAHAAABthADAowdbb9/AAAC6W1vb3YAAABsbXZoZAAAAAB8JbCAfCWwgAAAA+gAAAAAAAEAAAEAAAAAAAAAAAAAAAABAAAAAAAAAAAAAAAAAAAAAQAAAAAAAAAAAAAAAAAAQAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAIAAAIVdHJhawAAAFx0a2hkAAAAD3wlsIB8JbCAAAAAAQAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAABAAAAAAAAAAAAAAAAAAAAAQAAAAAAAAAAAAAAAAAAQAAAAAAIAAAACAAAAAABsW1kaWEAAAAgbWRoZAAAAAB8JbCAfCWwgAAAA+gAAAAAVcQAAAAAAC1oZGxyAAAAAAAAAAB2aWRlAAAAAAAAAAAAAAAAVmlkZW9IYW5kbGVyAAAAAVxtaW5mAAAAFHZtaGQAAAABAAAAAAAAAAAAAAAkZGluZgAAABxkcmVmAAAAAAAAAAEAAAAMdXJsIAAAAAEAAAEcc3RibAAAALhzdHNkAAAAAAAAAAEAAACobXA0dgAAAAAAAAABAAAAAAAAAAAAAAAAAAAAAAAIAAgASAAAAEgAAAAAAAAAAQAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAABj//wAAAFJlc2RzAAAAAANEAAEABDwgEQAAAAADDUAAAAAABS0AAAGwAQAAAbWJEwAAAQAAAAEgAMSNiB9FAEQBFGMAAAGyTGF2YzUyLjg3LjQGAQIAAAAYc3R0cwAAAAAAAAABAAAAAQAAAAAAAAAcc3RzYwAAAAAAAAABAAAAAQAAAAEAAAABAAAAFHN0c3oAAAAAAAAAEwAAAAEAAAAUc3RjbwAAAAAAAAABAAAALAAAAGB1ZHRhAAAAWG1ldGEAAAAAAAAAIWhkbHIAAAAAAAAAAG1kaXJhcHBsAAAAAAAAAAAAAAAAK2lsc3QAAAAjqXRvbwAAABtkYXRhAAAAAQAAAABMYXZmNTIuNzguMw=='));
+        'AAAAHGZ0eXBpc29tAAACAGlzb21pc28ybXA0MQAAAAhmcmVlAAAAG21kYXQAAAGzABAH' +
+            'AAABthADAowdbb9/AAAC6W1vb3YAAABsbXZoZAAAAAB8JbCAfCWwgAAAA+gAAAAA' +
+            'AAEAAAEAAAAAAAAAAAAAAAABAAAAAAAAAAAAAAAAAAAAAQAAAAAAAAAAAAAAAAAA' +
+            'QAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAIAAAIVdHJhawAAAFx0a2hk' +
+            'AAAAD3wlsIB8JbCAAAAAAQAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAABAAAAAAAA' +
+            'AAAAAAAAAAAAAQAAAAAAAAAAAAAAAAAAQAAAAAAIAAAACAAAAAABsW1kaWEAAAAg' +
+            'bWRoZAAAAAB8JbCAfCWwgAAAA+gAAAAAVcQAAAAAAC1oZGxyAAAAAAAAAAB2aWRl' +
+            'AAAAAAAAAAAAAAAAVmlkZW9IYW5kbGVyAAAAAVxtaW5mAAAAFHZtaGQAAAABAAAA' +
+            'AAAAAAAAAAAkZGluZgAAABxkcmVmAAAAAAAAAAEAAAAMdXJsIAAAAAEAAAEcc3Ri' +
+            'bAAAALhzdHNkAAAAAAAAAAEAAACobXA0dgAAAAAAAAABAAAAAAAAAAAAAAAAAAAA' +
+            'AAAIAAgASAAAAEgAAAAAAAAAAQAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA' +
+            'AAAAABj//wAAAFJlc2RzAAAAAANEAAEABDwgEQAAAAADDUAAAAAABS0AAAGwAQAA' +
+            'AbWJEwAAAQAAAAEgAMSNiB9FAEQBFGMAAAGyTGF2YzUyLjg3LjQGAQIAAAAYc3R0' +
+            'cwAAAAAAAAABAAAAAQAAAAAAAAAcc3RzYwAAAAAAAAABAAAAAQAAAAEAAAABAAAA' +
+            'FHN0c3oAAAAAAAAAEwAAAAEAAAAUc3RjbwAAAAAAAAABAAAALAAAAGB1ZHRhAAAA' +
+            'WG1ldGEAAAAAAAAAIWhkbHIAAAAAAAAAAG1kaXJhcHBsAAAAAAAAAAAAAAAAK2ls' +
+            'c3QAAAAjqXRvbwAAABtkYXRhAAAAAQAAAABMYXZmNTIuNzguMw=='));
 
-// Set method for keeping device awake.
-function keepAwakeSetting(mode) {
+/**
+ * Set method for keeping device awake.
+ *
+ * @param {string} mode The method to use.
+ */
+function keepAwakeSetting(mode) { // eslint-disable-line no-unused-vars
   keepAwakeMode = mode;
 }
-// Enable/Disable keeping device awake.
-function keepAwake(setting) {
+/**
+ * Enable/Disable keeping device awake.
+ * TODO: Fix pageload leak.
+ *
+ * @param {boolean} setting Enable or Disable.
+ */
+function keepAwake(setting) { // eslint-disable-line no-unused-vars
   try {
-    if (keepAwakeMode == "video") {
+    if (keepAwakeMode == 'video') {
       // Play a video to keep the device awake. (Chrome)
       setting ? video.play() : video.pause();
-    } else if (keepAwakeMode == "pageload") {
+    } else if (keepAwakeMode == 'pageload') {
       // Every 30s change the page then cancel page change. (Safari)
       setInterval(function() {
-        window.location.href = "/fake/page";
+        window.location.href = '/fake/page';
         window.setTimeout(window.stop);
       }, 30000);
     }
