178438e943e1747310a883352ec50e631f85ede5
Campbell Crowley <github@campbellcrowley.com>
2018-07-21 14:59:48 -0700

    sidebarcontroller.js passes eslint

--- sidebarcontroller.js
+++ sidebarcontroller.js
@@ -2,23 +2,23 @@
 // Author: Campbell Crowley (web@campbellcrowley.com)
 (function(Sidebar, undefined) {
 // Screen size (x/y) in pixels.
-var w = window, d = document, e = d.documentElement,
-    g = d.getElementsByTagName('body')[0],
-    x = w.innerWidth || e.clientWidth || g.clientWidth,
-    y = w.innerHeight || e.clientHeight || g.clientHeight;
+let w = window; let d = document; let e = d.documentElement;
+let g = d.getElementsByTagName('body')[0];
+let x = w.innerWidth || e.clientWidth || g.clientWidth;
+// let y = w.innerHeight || e.clientHeight || g.clientHeight;
 const minWidthToOpen = 1710;
 
-var isOpen = false;
+let isOpen = false;
 
-var sidebarDom;
+let sidebarDom;
 Sidebar.init = function() {
-  sidebarDom = document.getElementById("sidebar");
+  sidebarDom = document.getElementById('sidebar');
   if (x <= minWidthToOpen) {
-    sidebarOptionDoms = document.getElementsByClassName("sidebarOption");
-    for (var i = 0; i < sidebarOptionDoms.length; i++) {
-      var onClick = sidebarOptionDoms[i].onclick;
-      sidebarOptionDoms[i].addEventListener(
-          'click', function() { Sidebar.toggleOpen(false); });
+    sidebarOptionDoms = document.getElementsByClassName('sidebarOption');
+    for (let i = 0; i < sidebarOptionDoms.length; i++) {
+      sidebarOptionDoms[i].addEventListener('click', function() {
+        Sidebar.toggleOpen(false);
+      });
     }
   }
 };
@@ -26,7 +26,7 @@ Sidebar.init = function() {
 Sidebar.toggleOpen = function(force) {
   if (typeof force === 'undefined') {
     force = !isOpen;
-  } else if (force === "default") {
+  } else if (force === 'default') {
     if (x > minWidthToOpen) {
       force = true;
     } else {
@@ -34,7 +34,6 @@ Sidebar.toggleOpen = function(force) {
     }
   }
   isOpen = force;
-  sidebarDom.classList.toggle("open", isOpen);
+  sidebarDom.classList.toggle('open', isOpen);
 };
-
 }(window.Sidebar = window.Sidebar || {}));
