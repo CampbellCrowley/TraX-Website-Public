f782aa6b6c20915573574f7ae76fc79f3412680b
Campbell Crowley <github@campbellcrowley.com>
2018-07-21 23:27:30 -0700

    Remove .eslintrc.js from linting and docs

--- .hooks/make-docs.sh
+++ .hooks/make-docs.sh
@@ -8,6 +8,6 @@ GITROOT="$(git rev-parse --show-toplevel)"
 echo "Creating md files from jsdoc"
 JSDOC="$GITROOT/docs/README.md"
 
-JSINPUT="$(find -iname '*.js' -not -path './node_modules/*' -not -iname '*.min.js' -printf '%p ')"
+JSINPUT="$(find -iname '*.js' -not -path './node_modules/*' -not -iname '*.min.js' -not -iname '.eslintrc.js' -printf '%p ')"
 
 "$GITROOT/node_modules/.bin/jsdoc2md" "$JSINPUT" --private >> "$JSDOC"
