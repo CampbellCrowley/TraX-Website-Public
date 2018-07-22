e53b6f10e39759f3e7692fbb0c27ebbafd032927
Campbell Crowley <github@campbellcrowley.com>
2018-07-21 17:25:36 -0700

    paneController.js passes eslint

--- viewdata/paneController.js
+++ viewdata/paneController.js
@@ -1,55 +1,75 @@
 // Copyright 2018 Campbell Crowley. All rights reserved.
 // Author: Campbell Crowley (web@campbellcrowley.com)
 (function(Panes, undefined) {
-
 const swipeThreshold = 100; // Minimum pixels traveled to be considered swipe.
 const swipeDuration = 200; // Maximum milliseconds of finger down to be swipe.
 const dragElasticity = 1 / 60.0;  // Ratio of distance finger moved vs UI moves
                                   // to simulate elstic feel.
 
 // Finger down attributes.
-var swipeStartTime, startX, startY, cancelSwipe;
+let swipeStartTime;
+let startX;
+let startY;
+let cancelSwipe;
 // Currently visible pane index, and target to go to when panes open.
-var currentPane = -1, targetPane = 0;
+let currentPane = -1;
+let targetPane = 0;
 // Number of fingers currently placed on screen.
-var numFingers = 0;
+let numFingers = 0;
 
 // DOM Elements
-var paneContainerDom, panesList;
+let paneContainerDom;
+let panesList;
 
-var listeners = [];
+let listeners = [];
 
-// Initialize
+/**
+ * Initialize Panes
+ *
+ * @public
+ */
 Panes.init = function() {
-  paneContainerDom = document.getElementById("paneContainer");
-  panesList = document.getElementsByClassName("dataViewPane");
+  paneContainerDom = document.getElementById('paneContainer');
+  panesList = document.getElementsByClassName('dataViewPane');
 
-  panesList[0].classList.add("firstPane");
+  panesList[0].classList.add('firstPane');
 
   // TODO: Add {passive: true} as option for touchstart and touchend with
   // support for old browsers
   paneContainerDom.addEventListener('touchstart', handleTouchStart, false);
   paneContainerDom.addEventListener('touchmove', handleTouchMove, false);
   paneContainerDom.addEventListener('touchend', handleTouchEnd, false);
-  paneContainerDom.style.transition = "left 0.1s ease-in";
+  paneContainerDom.style.transition = 'left 0.1s ease-in';
 
   updatePanes();
 };
-// Panes are becoming visible.
+/**
+ * Panes are becoming visible.
+ *
+ * @public
+ */
 Panes.handleOpening = function() {
   Panes.setCurrent(0);
-  viewDataOverlay.style.right = "0";
+  viewDataOverlay.style.right = '0';
   setTimeout(animatePanes(), 200);
 };
-// Panes are transitioning to no longer visible.
+/**
+ * Panes are transitioning to no longer visible.
+ *
+ * @public
+ */
 Panes.handleClosing = function() {
   targetPane = currentPane;
   Panes.setCurrent(0);
   updatePanes();
-  viewDataOverlay.style.right = "-100%";
+  viewDataOverlay.style.right = '-100%';
   TraX.leaveDataView(false);
 };
-// Animate transition to targetPane from currentPane.
+/**
+ * Animate transition to targetPane from currentPane.
+ *
+ * @private
+ */
 function animatePanes() {
   if (targetPane < 0) targetPane = 0;
   if (targetPane >= panesList.length) targetPane = panesList.length - 1;
@@ -67,66 +87,105 @@ function animatePanes() {
     }, 100);
   }
 }
-// Set each pane class appropriately as it will appear.
+/**
+ * Set each pane class appropriately as it will appear.
+ *
+ * @private
+ */
 function updatePanes() {
   if (currentPane < 0) {
     Panes.setCurrent(0);
   } else {
-    viewDataOverlay.style.right = "0";
+    viewDataOverlay.style.right = '0';
   }
   if (currentPane >= panesList.length) currentPane = panesList.length - 1;
-  for (var i = 0; i < panesList.length; i++) {
-    panesList[i].classList.remove("visiblePane");
-    panesList[i].classList.remove("nextPane");
-    panesList[i].classList.remove("previousPane");
-    panesList[i].classList.remove("farRightPane");
-    panesList[i].classList.remove("farLeftPane");
+  for (let i = 0; i < panesList.length; i++) {
+    panesList[i].classList.remove('visiblePane');
+    panesList[i].classList.remove('nextPane');
+    panesList[i].classList.remove('previousPane');
+    panesList[i].classList.remove('farRightPane');
+    panesList[i].classList.remove('farLeftPane');
     if (i == currentPane) {
-      panesList[i].classList.add("visiblePane");
+      panesList[i].classList.add('visiblePane');
     } else if (i - 1 == currentPane) {
-      panesList[i].classList.add("nextPane");
+      panesList[i].classList.add('nextPane');
     } else if (i + 1 == currentPane) {
-      panesList[i].classList.add("previousPane");
+      panesList[i].classList.add('previousPane');
     } else if (i > currentPane) {
-      panesList[i].classList.add("farRightPane");
+      panesList[i].classList.add('farRightPane');
     } else if (i < currentPane) {
-      panesList[i].classList.add("farLeftPane");
+      panesList[i].classList.add('farLeftPane');
     }
   }
 }
-// Sets the current index for panes but does not trigger change in panes. Used
-// for setting default pane.
+/**
+ * Sets the current index for panes but does not trigger change in panes. Used
+ * for setting default pane.
+ *
+ * @public
+ * @param {number} index Index of pane to set to.
+ */
 Panes.setCurrent = function(index) {
   currentPane = index;
 };
-// Go to next pane.
+/**
+ * Go to next pane.
+ *
+ * @public
+ */
 Panes.nextPane = function() {
   Panes.gotoPane(currentPane + 1);
 };
-// Go to previous pane.
+/**
+ * Go to previous pane.
+ *
+ * @public
+ */
 Panes.previousPane = function() {
   Panes.gotoPane(currentPane - 1);
 };
-// Go to pane with given index.
+/**
+ * Go to pane with given index.
+ *
+ * @public
+ * @param {number} index Index of pane to go to.
+ */
 Panes.gotoPane = function(index) {
-  if (index != currentPane)
+  if (index != currentPane) {
     triggerEvent('changePane', {fromIndex: currentPane, toIndex: index});
+  }
 
   currentPane = index;
   if (currentPane < 0) Panes.handleClosing();
   else updatePanes();
 };
-// Get current pane index.
+/**
+ * Get current pane index.
+ *
+ * @public
+ * @return {number} Index of currently visible pane.
+ */
 Panes.getPane = function() {
   return currentPane;
 };
-// User swiped, change to next or previous pane.
+/**
+ * User swiped, change to next or previous pane.
+ *
+ * @private
+ * @param {boolean} isRightSwipe Was the swipe towrds the right.
+ * @param {number} [startY] The Y coordinate of the screen the touch started on.
+ */
 function handleSwipe(isRightSwipe, startY) {
   isRightSwipe ? Panes.previousPane() : Panes.nextPane();
 }
-// Finger is placed on screen.
+/**
+ * Finger is placed on screen.
+ *
+ * @private
+ * @param {Event} event The touch event.
+ */
 function handleTouchStart(event) {
-  var touchobj = event.changedTouches[0];
+  let touchobj = event.changedTouches[0];
   numFingers++;
 
   // If there is more than one finger, cancel moving screen and swipe.
@@ -141,20 +200,26 @@ function handleTouchStart(event) {
   cancelSwipe = false;
   swipeStartTime = Date.now();
 }
-// Finger moves, move UI to simulate dragging unless swipe is canceled.
+/**
+ * Finger moves, move UI to simulate dragging unless swipe is canceled.
+ *
+ * @private
+ * @param {Event} event The touch event.
+ */
 function handleTouchMove(event) {
-  var touchobj = event.changedTouches[0];
-  var distX = touchobj.pageX - startX;
-  var distY = touchobj.pageY - startY;
-  paneContainerDom.style.transition = "";
+  let touchobj = event.changedTouches[0];
+  let distX = touchobj.pageX - startX;
+  let distY = touchobj.pageY - startY;
+  paneContainerDom.style.transition = '';
   // Cancel if swipe takes too long.
-  if (!cancelSwipe && Date.now() - swipeStartTime > swipeDuration)
+  if (!cancelSwipe && Date.now() - swipeStartTime > swipeDuration) {
     cancelSwipe = true;
+  }
 
   if (!cancelSwipe && Math.abs(distX) > 10 &&
       Math.abs(distX) > Math.abs(distY)) {
     paneContainerDom.style.left =
-        1.0 / (Math.abs(distX) * dragElasticity + 1) * distX + "px";
+        1.0 / (Math.abs(distX) * dragElasticity + 1) * distX + 'px';
   } else {
     paneContainerDom.style.left = 0;
     if (Math.abs(distX) > 10 && distX < distY) {
@@ -162,31 +227,50 @@ function handleTouchMove(event) {
     }
   }
 }
-// Finger is removed from screen, reset screen position if not a swipe, or
-// handle swipe if it was the end of a valid swipe.
+/**
+ * Finger is removed from screen, reset screen position if not a swipe, or
+ * handle swipe if it was the end of a valid swipe.
+ *
+ * @private
+ * @param {Event} event The touch event.
+ */
 function handleTouchEnd(event) {
   fingerDown = false;
   numFingers--;
-  var touchobj = event.changedTouches[0];
+  let touchobj = event.changedTouches[0];
   paneContainerDom.style.left = 0;
-  paneContainerDom.style.transition = "left 0.1s ease-in";
-  var distX = touchobj.pageX - startX;
-  var distY = touchobj.pageY - startY;
+  paneContainerDom.style.transition = 'left 0.1s ease-in';
+  let distX = touchobj.pageX - startX;
+  let distY = touchobj.pageY - startY;
   elapsedTime = Date.now() - swipeStartTime;
-  var swiped = !cancelSwipe && elapsedTime <= swipeDuration &&
+  let swiped = !cancelSwipe && elapsedTime <= swipeDuration &&
       Math.abs(distX) >= swipeThreshold;
   if (swiped) {
     event.preventDefault();
-    var swipedRight = distX > 0 && distY < distX;
+    let swipedRight = distX > 0 && distY < distX;
     handleSwipe(swipedRight);
   }
 }
 
+/**
+ * Add a listener to a Panes Event.
+ *
+ * @public
+ * @param {string} name Name of event to listen for.
+ * @param {paneEventCB} callback Function to fire when event fires.
+ */
 Panes.addEventListener = function(name, callback) {
   listeners.push({name: name, callback: callback});
 };
+/**
+ * Trigger a Panes Event for all listeners.
+ *
+ * @private
+ * @param {string} name Name of event to fire.
+ * @param {*} data Data to send with event.
+ */
 function triggerEvent(name, data) {
-  for (var i = 0; i < listeners.length; i++) {
+  for (let i = 0; i < listeners.length; i++) {
     if (listeners[i].name == name) {
       listeners[i].callback(data);
     }
