9230f6b26e272637d75aa31a5c3c1ccbd43be7b3
Campbell Crowley <github@campbellcrowley.com>
2019-04-04 12:14:47 -0700

    ./scriptloader.js passes linting

--- scriptloader.js
+++ scriptloader.js
@@ -1,4 +1,4 @@
-// Copyright 2018 Campbell Crowley. All rights reserved.
+// Copyright 2018-2019 Campbell Crowley. All rights reserved.
 // Author: Campbell Crowley (web@campbellcrowley.com)
 
 /**
@@ -7,104 +7,104 @@
  * @module ScriptLoader/
  */
 (function() {
-window.onload = preinit;
+  window.onload = preinit;
 
-/**
- * Additional scripts to fetch after page has loaded.
- *
- * @default
- * @constant
- * @private
- * @type {Array.<{url: string, init: ?Array.<string>}>}
- */
-const additionalScripts = [
-  {
-    url: 'https://dev.campbellcrowley.com/trax/traxCommon.js',
-    init: ['TraX', 'Common'],
-  },
-  {
-    url: 'https://dev.campbellcrowley.com/trax/pagecontroller.js',
-    init: ['TraX'],
-  },
-  {
-    url: 'https://dev.campbellcrowley.com/trax/canvascontroller.js',
-    init: ['TraX', 'Canvases'],
-  },
-  {url: 'https://dev.campbellcrowley.com/trax/constants.js'},
-  {url: 'https://dev.campbellcrowley.com/trax/keepAwake.js'},
-  {url: 'https://dev.campbellcrowley.com/trax/lz-string.min.js'},
-  {
-    url: 'https://dev.campbellcrowley.com/trax/videocontroller.js',
-    init: ['TraX', 'Video'],
-  },
-  {url: 'https://dev.campbellcrowley.com/socket.io.js'},
-  {
-    url: 'https://dev.campbellcrowley.com/trax/sidebarcontroller.js',
-    init: ['Sidebar'],
-  },
-  {url: 'https://c6.patreon.com/becomePatronButton.bundle.js', nowait: true},
-];
-let numInit = 0;
+  /**
+   * Additional scripts to fetch after page has loaded.
+   *
+   * @default
+   * @constant
+   * @private
+   * @type {Array.<{url: string, init: ?Array.<string>}>}
+   */
+  const additionalScripts = [
+    {
+      url: 'https://dev.campbellcrowley.com/trax/traxCommon.js',
+      init: ['TraX', 'Common'],
+    },
+    {
+      url: 'https://dev.campbellcrowley.com/trax/pagecontroller.js',
+      init: ['TraX'],
+    },
+    {
+      url: 'https://dev.campbellcrowley.com/trax/canvascontroller.js',
+      init: ['TraX', 'Canvases'],
+    },
+    {url: 'https://dev.campbellcrowley.com/trax/constants.js'},
+    {url: 'https://dev.campbellcrowley.com/trax/keepAwake.js'},
+    {url: 'https://dev.campbellcrowley.com/trax/lz-string.min.js'},
+    {
+      url: 'https://dev.campbellcrowley.com/trax/videocontroller.js',
+      init: ['TraX', 'Video'],
+    },
+    {url: 'https://dev.campbellcrowley.com/socket.io.js'},
+    {
+      url: 'https://dev.campbellcrowley.com/trax/sidebarcontroller.js',
+      init: ['Sidebar'],
+    },
+    {url: 'https://c6.patreon.com/becomePatronButton.bundle.js', nowait: true},
+  ];
+  let numInit = 0;
 
-/**
- * Fetch additional necessary scripts.
- *
- * @private
- */
-function preinit() {
-  let head = document.getElementsByTagName('head')[0];
-  for (let i = 0; i < additionalScripts.length; i++) {
-    let newScript = document.createElement('script');
-    newScript.src = additionalScripts[i].url;
-    newScript.type = 'text/javascript';
-    if (additionalScripts[i].nowait) {
-      numInit++;
-    } else {
-      newScript.onload = handleScriptLoad;
+  /**
+   * Fetch additional necessary scripts.
+   *
+   * @private
+   */
+  function preinit() {
+    const head = document.getElementsByTagName('head')[0];
+    for (let i = 0; i < additionalScripts.length; i++) {
+      const newScript = document.createElement('script');
+      newScript.src = additionalScripts[i].url;
+      newScript.type = 'text/javascript';
+      if (additionalScripts[i].nowait) {
+        numInit++;
+      } else {
+        newScript.onload = handleScriptLoad;
+      }
+      head.appendChild(newScript);
     }
-    head.appendChild(newScript);
   }
-}
-/**
- * A script loaded.
- *
- * @private
- */
-function handleScriptLoad() {
-  numInit++;
-  if (numInit < additionalScripts.length) return;
-  console.log('INIT: begin');
-  for (let i = 0; i < additionalScripts.length; i++) {
-    if (typeof additionalScripts[i].init !== 'object') continue;
-    console.log('INIT:', additionalScripts[i].init);
-    try {
-      getScriptRef(window, additionalScripts[i].init, 0).init();
-    } catch (e) {
-      console.error('Failed to init', additionalScripts[i].init, e);
-      if (TraX && TraX.showMessageBox) {
-        TraX.showMessageBox(
-            'Failed to load scripts! Only data collection may work!', 30000,
-            true);
+  /**
+   * A script loaded.
+   *
+   * @private
+   */
+  function handleScriptLoad() {
+    numInit++;
+    if (numInit < additionalScripts.length) return;
+    console.log('INIT: begin');
+    for (let i = 0; i < additionalScripts.length; i++) {
+      if (typeof additionalScripts[i].init !== 'object') continue;
+      console.log('INIT:', additionalScripts[i].init);
+      try {
+        getScriptRef(window, additionalScripts[i].init, 0).init();
+      } catch (e) {
+        console.error('Failed to init', additionalScripts[i].init, e);
+        if (TraX && TraX.showMessageBox) {
+          TraX.showMessageBox(
+              'Failed to load scripts! Only data collection may work!', 30000,
+              true);
+        }
       }
     }
+    console.log('INIT: complete');
   }
-  console.log('INIT: complete');
-}
-/**
- * Gets a reference to the requested script object via nemspace names.
- *
- * @private
- * @param {Object} win The parent obeject to start looking in. Usually window.
- * @param {Array.<string>} namespaces Array of namespace names to recurse
- * through.
- * @param {number} index The array index to start looking at.
- * @return {Object} Reference to the requested object.
- */
-function getScriptRef(win, namespaces, index) {
-  if (index < namespaces.length) {
-    return getScriptRef(win[namespaces[index]], namespaces, ++index);
-  } else {
-    return win;
+  /**
+   * Gets a reference to the requested script object via nemspace names.
+   *
+   * @private
+   * @param {Object} win The parent obeject to start looking in. Usually window.
+   * @param {Array.<string>} namespaces Array of namespace names to recurse
+   * through.
+   * @param {number} index The array index to start looking at.
+   * @return {Object} Reference to the requested object.
+   */
+  function getScriptRef(win, namespaces, index) {
+    if (index < namespaces.length) {
+      return getScriptRef(win[namespaces[index]], namespaces, ++index);
+    } else {
+      return win;
+    }
   }
-}
 })();
